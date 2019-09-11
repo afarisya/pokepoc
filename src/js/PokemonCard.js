856389a2e8cf4304c9e636f6822b34190ad129d8
Farisya Adzkia <adzkiafa@gmail.com>
2019-09-11 10:57:46 +0700

    Show release button if pokemon is catched

--- src/js/PokemonCard.js
+++ src/js/PokemonCard.js
@@ -5,17 +5,30 @@ import {
     CardImg, 
     CardBody,
     CardTitle,
-    Badge
+    Badge,
+    Modal, 
+    ModalHeader, 
+    ModalBody, 
+    ModalFooter,
+    Button,
 } from 'reactstrap';
+import store from '../store';
   
 import { _parseJSON } from '../utils/_parseJSON';
+import { releasePokemon } from '../utils/IndexedDb';
+
+import { reqMyPokemonList } from '../reducers/MyPokemonListReducers';
 
 class PokemonCard extends React.Component {    
     constructor(props) {
         super(props);
         this.state = {
+            modal           : false,    // modal
+            
             picture : [],
         }
+
+        this.modalToggle = this.modalToggle.bind(this);
     }
     
     componentDidMount() {
@@ -48,7 +61,36 @@ class PokemonCard extends React.Component {
     
     }
 
+    modalToggle() {
+        this.setState({
+            modal: !this.state.modal
+        });
+    }
+
+    releaseBtnOnClick = (e) => {
+        e.preventDefault();
+        this.modalToggle(); 
+    }
+
+    releasePokemonOnClick = () => {
+        // Add to MyPokemonList
+        releasePokemon(this.props.id)
+            .then(data => {
+                this.modalToggle(); 
+                store.dispatch(reqMyPokemonList(store.getState().MyPokemonListReducers.activePage))
+            })
+            .catch(err => {
+                console.log(err)
+                this.modalToggle();
+            });
+    }
+
     render() {
+        var catched = false;
+        if ( typeof this.props.catched !== "undefined" ) {
+            catched = this.props.catched;
+        }
+        
         return (
             <NavLink to={`/pokemon/${this.props.id}`}>
                 <Card>
@@ -64,7 +106,40 @@ class PokemonCard extends React.Component {
                     </CardTitle>
                     {/* <CardSubtitle>Card subtitle</CardSubtitle>
                     <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText> */}
-                    {/* <Button>Button</Button> */}
+                    { catched ?
+                        <React.Fragment>
+                            <Button 
+                                className="release-btn" 
+                                color="warning" 
+                                onClick={this.releaseBtnOnClick}
+                            >
+                                Release
+                            </Button>
+                            <Modal 
+                                isOpen={this.state.modal} 
+                                toggle={this.modalToggle}
+                                backdrop="static" 
+                                id="catch-pokemon-modal"
+                            >
+                                <ModalBody>
+                                    <h5>Are you sure you want to release <span style={{fontWeight: "bold"}}>{this.props.nickname} the {this.props.name}</span>?</h5>
+                                    <Button 
+                                        color="secondary"
+                                        onClick={this.modalToggle}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button 
+                                        color="primary"
+                                        onClick={this.releasePokemonOnClick}
+                                    >
+                                        Release
+                                    </Button>
+                                </ModalBody>
+                            </Modal>
+                        </React.Fragment>
+                        : null
+                    }
                     </CardBody>
                 </Card>
             </NavLink>
